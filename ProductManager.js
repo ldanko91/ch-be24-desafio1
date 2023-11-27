dccfba5c1a0459cd07ddbcb1da79aea541686234
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2023-11-27 19:44:45 -0300

    Desafio1.1

--- ProductManager.js
+++ ProductManager.js
@@ -1,4 +1,3 @@
-let Productos = [{id:1, title:"prueba1", description:"producto de pruebas", price:100, thumbnail:"foto.jpg", code:"abc123", stock:10},{id:2, title:"prueba2", description:"producto de pruebas 2", price:110, thumbnail:"foto.jpg", code:"abc1234", stock:11}];
 class ProductManager {
     constructor (title, description, price, thumbnail, code, stock) {
         this.title=title
@@ -6,7 +5,8 @@ class ProductManager {
         this.price=price
         this.thumbnail=thumbnail
         this.code=code
-        this.stock=stock   
+        this.stock = stock
+        this.products = []   
     }
 
 
@@ -14,12 +14,12 @@ class ProductManager {
         if(!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Todos los campos son obligatorios!")
         }else{
-            if(Productos.some(producto => producto.code === code )){
+            if (this.products.some(producto => producto.code === code)) {
                 console.log("El campo code ya existe!")
             }else{
             let id
             let idAuto = () => {
-                let maxId = Productos.length
+                let maxId = this.products.length
                 id = (maxId + 1)
                 return id
             }
@@ -34,35 +34,35 @@ class ProductManager {
                 code:code,
                 stock: stock,
             }
-            return Productos.push(newProd)
+                return this.products.push(newProd)
     }
     }
     }
     
 
     getProducts = () => {
-        console.table(Productos)
+        console.table(this.products)
     }
 
     getProductById = (prodId) => {
-        if(!Productos.some(producto => producto.id === prodId )){
+        if (!this.products.some(producto => producto.id === prodId)) {
             console.log("El producto buscado no existe!")
         }else{
 
-        let prodIndex = Productos.findIndex(producto => producto.id === prodId)
-        return console.table(Productos[prodIndex])
+            let prodIndex = this.products.findIndex(producto => producto.id === prodId)
+            return console.table(this.products[prodIndex])
         }
     }  
 }
 
 const Manager1 = new ProductManager;
 
-//Muestra inicial
-Manager1.getProducts()
+// //Muestra inicial
+// Manager1.getProducts()
 
-//Prueba código repetido
-console.log("Prueba producto repetido")
-Manager1.addProduct("prueba1", "producto de pruebas", 100, "foto.jpg", "abc123", 10)
+// //Prueba código repetido
+// console.log("Prueba producto repetido")
+// Manager1.addProduct("prueba1", "producto de pruebas", 100, "foto.jpg", "abc123", 10)
 
 //Prueba atributo faltante
 console.log("Prueba atributo faltante")
@@ -71,6 +71,7 @@ Manager1.addProduct("prueba1", "producto de pruebas", 100,  "abz123", 10)
 //Añadir productos
 Manager1.addProduct("añadido1", "description1", 125, "foto2.jpg", "azz123", 5)
 Manager1.addProduct("añadido2", "description2", 250, "foto332.jpg", "a5z123", 15)
+Manager1.addProduct("añadido3", "description3", 1225, "foto1222.jpg", "azz1423", 22)
 
 //Mostrar productos añadidos
 Manager1.getProducts()
